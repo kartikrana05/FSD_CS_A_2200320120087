86850ca4d8e095f0a956bac161051b2fed26d20b
KARTIK RANA <kartikrana3332@gmail.com>
2024-10-16 14:16:21 +0530

    addes js folder

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-let a=12;
-if(a>10){
-    a=30;
-    console.log(a);
-}
-console.log(a)
-console.log(Number.MAX_VALUE)
-console.log(BigInt.MAX_VALUE)
-log
+// let a=12;
+// if(a>10){
+//     a=30;
+//     console.log(a);
+// }
+// console.log(a)
+// console.log(Number.MAX_VALUE)
+// console.log(BigInt.MAX_VALUE)
+// log
+let a=Symbol.for("x")
+console.log(Symbol.keyFor(a));
+let person={name:"xyz",
+    age:30
+};
+let car=['maruti','honda','TATA']
+console.log(typeof(car));
+console.log(typeof(person));
+let person1=new Object();
+person1.name="xyz"
+person1.age=30
+
+
+
---
+++ io.js
@@ -0,0 +1,11 @@
+function hello() {
+    document.write("hello users")
+}
+hello()
+function add(a,b){
+    return (a+b);
+}
+let sum=add(10,20)
+document.write(sum)
+let sum1=add;
+document.write(sum1)
